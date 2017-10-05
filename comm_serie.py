233513f1e6c2de8bd424c7e12500a400bb661582
Charles DELCROIX <charlesdelcroix@outlook.com>
2017-10-05 09:15:19 +0200

    Ajout de fonctions

--- comm_serie.py
+++ comm_serie.py
@@ -1,17 +1,31 @@
-from serial import *
 import time
+from serial import *
 
-motor_g_d_direction = 97
-motor_g_speed = 97
-motor_d_speed = 97
+global direction
+global pwm1
+global pwm2
 
-with Serial(port="/dev/ttyUSB2", baudrate=9600, timeout=1, writeTimeout=1) as port_serie:
+direction = 0
+pwm1 = 0
+pwm2 = 0
+
+global port_serie
+
+port_serie = Serial(port="/dev/ttyUSB0", baudrate=9600, timeout=1, writeTimeout=1)
+
+def serial_init():
 	while port_serie.read() != 'z':
 		pass
-	print('a')
-	direction = chr(motor_g_d_direction)
-	pwm1 = chr(motor_g_speed)
-	pwm2 = chr(motor_d_speed)
-	nombre = ''.join([direction,pwm1,pwm2])
-	port_serie.write(nombre.encode('ascii'))
+
+def serial_close():
+	commande(0,0,0)
+	time.sleep(1)
 	port_serie.close()
+
+def commande(motor_g_d_direction,motor_g_speed,motor_d_speed):
+	direction = motor_g_d_direction
+	pwm1 = motor_g_speed
+	pwm2 = motor_d_speed
+	nombre = ''.join([chr(direction),chr(pwm1),chr(pwm2)])
+	port_serie.write(nombre.encode('ascii'))
+		
